9429205527874def8884357479aa29e8f6cb8198
emakarchuk <EIMakarchuk@sberbank.ru>
2021-01-22 14:58:08 +0300

    final

--- dlq/implementation.py
+++ dlq/implementation.py
@@ -11,126 +11,104 @@ __all__ = [
 VT = t.TypeVar("VT")
 
 class DLQ(abstract.AbstractDLQ):
-    def __init__(self, value = None):
-        #abstract.AbstractDLQ.__init__(self, value)
-        '''i = isinstance(value, list)
-        if i is True:
-            for n in value:
-                self.head = DLQItem(n)
-                #print(self.head.value)
-                self.head = self.head.next'''
-                #print("x")
-            #print(i)
-        #else:
-        self.head = DLQItem(value)
+    
+    def __init__(self, initializer: t.Optional[t.Iterable[VT]] = None) -> None:
+        super().__init__()
+        self.head: t.Optional[DLQItem] = None
+        if initializer is not None:
+            prev_dlq_item: t.Optional[DLQItem] = None
+            for value in initializer:
+                item = DLQItem(value)
+                if prev_dlq_item is None:
+                    self.head = item
+                else:
+                    prev_dlq_item.next_item = item
+                    item.previous_item = prev_dlq_item
+                prev_dlq_item = item
+
     def first(self):
-        '''if self.previous is not None:
-            self.value = self.previous'''
-        #self.next.value = 2
         current = self.head
-        print("first is:",current.value)
         return current
+
     def last(self):
-        curr = self.head
-        while curr.next is not None:
-            curr = curr.next
-        print("Last item is ",curr.value)
-        return curr
+        curr: t.Optional[DLQItem] = self.head
+        prev: t.Optional[DLQItem] = None
+        while curr is not None:
+            prev = curr
+            curr = curr.next_item
+        return prev
+
     def append(self, value):
-        #node = DLQItem(value)
-        #node.next = None
         if self.head is None:
             node = DLQItem(value)
-            node.previous = None
+            node.previous_item = None
             self.head = node
-            #return
         else:
             node = DLQItem(value)
             curr = self.head
-            while curr.next is not None:
-                curr = curr.next
-            curr.next = node
-            node.previous = curr
-            node.next = None
-            #return
+            while curr.next_item is not None:
+                curr = curr.next_item
+            curr.next_item = node
+            node.previous_item = curr
+            node.next_item = None
+
     def appendleft(self, value):
         if self.head is None:
             node = DLQItem(value)
-            node.previous = None
+            node.previous_item = None
             self.head = node
         else:
             node = DLQItem(value)
-            self.head.previous = node
-            node.next = self.head
+            self.head.previous_item = node
+            node.next_item = self.head
             self.head = node
-            node.previous = None
+            node.previous_item = None
+
     def pop(self):
-            #try:
             if self.head is None:
-                print("Error1")
-                raise Error("KeyError on emptiness")
-            else:
-                curr = self.head
-                while curr.next is not None:
-                    curr = curr.next
-                last = curr # last element in a list
-                new_last = curr.previous
-                print("new last value", new_last.next.value)
-                new_last.next = None
-                last.previous = None
-                self.head.previous = new_last
-                return last
-            #except Exception as ex:
-            #    print("Error")
+                raise KeyError
+            curr = self.head
+            while curr.next_item is not None:
+                curr = curr.next_item
+            last = curr
+            new_last = curr.previous_item
+            new_last.next_item = None
+            last.previous_item = None
+            return last.value
+
     def popleft(self):
         if self.head is None:
-                print("Error1")
-                raise Error("KeyError on emptiness")
-        else:
-            curr = self.head
-            first = curr # first element in a list
-            new_first = curr.next
-            new_first.previous = None
-            first.next = None
-            #self.head.next = new_first
-            self.head = new_first
-            print("New first value:", new_first.value)
-            return first
+                raise KeyError
+        curr = self.head
+        first = curr
+        new_first = curr.next_item
+        new_first.previous_item = None
+        first.next_item = None
+        self.head = new_first
+        return first.value
+
+    def __eq__(self, new_list):
+        return list(self) == list(new_list)
+
     def __iter__(self):
         node = self.head
         while node is not None:
             yield node.value
-            node = node.next
-    def print_list(self):
-        curr = self.head
-        print("List:")
-        while curr is not None:
-            print(curr.value)
-            curr = curr.next
+            node = node.next_item
 
 class DLQItem(abstract.AbstractDLQItem):
     def __init__(self, value: VT):
         self._value = value
-        self.next = None
-        self.previous = None
+        self.next_item = None
+        self.previous_item = None
+
     @property
     def value(self):
         return self._value
+
     def next(self):
-        return self.next
+        return self.next_item
+
     def previous(self):
-        return self.previous
+        return self.previous_item
         
-dllist = DLQ(1)
-dllist.append(2)
-dllist.first()
-dllist.last()
-dllist.appendleft(0)
-#dllist.first()
-dllist.append(3)
-dllist.last()
-dllist.print_list()
-dllist.pop()
-dllist.print_list()
-dllist.popleft()
-dllist.print_list()
