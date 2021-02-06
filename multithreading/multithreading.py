5886f1aa8311365425a0bd0cffa1b26a95a434ab
emakarchuk <EIMakarchuk@sberbank.ru>
2021-02-06 18:03:24 +0300

    done

---
+++ multithreading/multithreading.py
@@ -0,0 +1,13 @@
+import time
+import threading
+from threading import Thread
+
+def runner(duration: float) -> None:
+    time.sleep(duration)
+    print(f"Successfully finished after {duration} seconds of sleeping")
+
+n = 1
+while n <= 10:
+    t = Thread(target=runner, args=(n,))
+    t.start()
+    n = n + 1
