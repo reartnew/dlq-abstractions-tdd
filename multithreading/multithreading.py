3438f07f1742428cedff0d3b4621a1ac8bcdb0ca
emakarchuk <EIMakarchuk@sberbank.ru>
2021-02-11 20:14:25 +0300

    .

--- multithreading/multithreading.py
+++ multithreading/multithreading.py
@@ -3,30 +3,32 @@ import threading
 import math
 from threading import Thread
 
-def threading_decorator(function):
-    
-    def wrap_in_thread(res_list, i, n):
-        t = Thread(target=function, args=(res_list, i, n,))
+# сделать его сигнатуру не зависящей от декорируемой функции
+
+def wrap_in_thread(function):
+   
+    def threading_decorator(*arg, **kwarg):
+
+        t = Thread(target=function, args=arg, kwargs=kwarg)
         t.start()
         return t
    
-    return wrap_in_thread
+    return threading_decorator
 
-@threading_decorator
+@wrap_in_thread
 def runner(res_list, i, duration: float) -> None:
     time.sleep(duration)
     print(f"Successfully finished after {duration} seconds of sleeping")
     # return math.sin(duration)
     res_list[i] = math.sin(duration)
 
-n = 1
 thread_list = []
 res_list = [None] * 10
 i = 0
-while n <= 10:
+
+for n in range(1, 11):
     t = runner(res_list, i, n)
     thread_list.append(t)
-    n = n + 1
     i = i + 1
 
 for th in thread_list:
