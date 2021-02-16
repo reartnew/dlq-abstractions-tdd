b827beb8e1032c28661d9f8c52f8c23f96f9a542
emakarchuk <EIMakarchuk@sberbank.ru>
2021-02-16 14:12:08 +0300

    extended threads

--- multithreading/multithreading.py
+++ multithreading/multithreading.py
@@ -3,34 +3,34 @@ import threading
 import math
 from threading import Thread 
 
+class thread_extended(threading.Thread):
+    
+    def get_result(self):
+        return res_list
+
 def wrap_in_thread(function):
    
     def threading_decorator(*arg, **kwarg):
-
-        t = Thread(target=function, args=arg, kwargs=kwarg)
+        t = thread_extended(target=function, args=arg, kwargs=kwarg)
         t.start()
         return t
    
     return threading_decorator
 
 @wrap_in_thread
-def runner(res_list, i, duration: float) -> None:
+def runner(duration: float) -> None:
     time.sleep(duration)
     print(f"Successfully finished after {duration} seconds of sleeping")
-    # return math.sin(duration)
-    res_list[i] = math.sin(duration)
+    res_list[duration - 1] = math.sin(duration)
 
 thread_list = []
 res_list = [None] * 10
-i = 0
 
-for n in range(1, 11):
-    t = runner(res_list, i, n)
-    thread_list.append(t)
-    i = i + 1
+thread_list = [runner(i) for i in range(1,11)]
 
 for th in thread_list:
     th.join()
+
 print("Number of active threads is", threading.active_count())
-print (res_list)
+print(thread_list[0].get_result())
 print("All threads are finished")
